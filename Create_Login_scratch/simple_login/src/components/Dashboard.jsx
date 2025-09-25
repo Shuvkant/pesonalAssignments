f4e284800a813a777e9f85e3345482ec235e1541
Shuvkant <weird426344@gmail.com>
2025-09-26 02:43:28 +0545

    testing branch

--- Create_Login_scratch/simple_login/src/components/Dashboard.jsx
+++ Create_Login_scratch/simple_login/src/components/Dashboard.jsx
@@ -5,6 +5,7 @@ const Dashboard = ({ name }) => {
       <div className="flex flex-col gap-4">
         <h4>You have successfully loggedin.</h4>
         Welcome to Dashboard <strong className="font-bold text-red-300 font-serif ">{name || User}</strong>
+        <p>You are Genius</p>
       </div>
     </>
   )
