91de020123849d2e648bc5388fde29e96b818ca1
Shuvkant <weird426344@gmail.com>
2025-09-29 13:51:57 +0545

    adding docker file

--- Create_Login_scratch/simple_login/vite.config.js
+++ Create_Login_scratch/simple_login/vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // server: {
+  //   port: 5173, // Port for the development server (vite)
+  // },
+  // preview: {
+  //   port: 3000, // Port for the preview server (vite preview)
+  // },
 })
