4d88487f12b57e20d29f753b4d5c5acf0f4452cd
Shuvkant <weird426344@gmail.com>
2025-10-02 23:03:27 +0545

    news initialized

---
+++ newsWebsite/src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <div className='flex flex-col justify-around'>
+        <div className='flex flex-row justify-around'>
+          <div className='font-bold text-green'>Home</div>
+          <div>About</div>
+          <div>contact</div>
+          <div>Services</div>
+        </div>
+        <div>
+          <div>
+            <h1>Nepal</h1>
+            <p>Nepal ko maya</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
